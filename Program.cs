4d15f37f27ca99d28d1112b9403457b95ae38037
Katsushige Yoshida <k-yosida@g06.itscom.net>
2024-08-19 08:05:42 +0900

    ファイル選択修正

--- Program.cs
+++ Program.cs
@@ -2,30 +2,48 @@
 using CoreLib;
 using KScript;
 
-YLib ylib = new YLib();
-
-Console.WriteLine("KScript Test");
-const string PROMPT = ">> ";
-while (true) {
-    scriptTest();
-    Console.Write($"{"\n"} {PROMPT}");
-    var input = Console.ReadLine();
-    if (input == "q") return;
-    //if (string.IsNullOrEmpty(input)) return;
-
-}
-
-void scriptTest()
+class KScrpt
 {
+    static YLib ylib = new YLib();
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("KScript Test");
+        const string PROMPT = ">> ";
+        while (true) {
+            scriptTest();
+            Console.Write($"{"\n"} {PROMPT}");
+            var input = Console.ReadLine();
+            if (0 < input.Length && input[0] == 'q') return;
+            //if (string.IsNullOrEmpty(input)) return;
+        }
+    }
+
     //  Script Test
-    string scriptFile = ylib.consoleFileSelect("..\\..\\..\\TestData", "*.sc");
-    string scriptData = ylib.loadTextFile(scriptFile);
+    static void scriptTest()
+    {
+        //  既定フォルダの取得
+        KScript.Properties.Settings.Default.Reload();
+        string dataFolder = KScript.Properties.Settings.Default.DataFolder;
+        if (!Directory.Exists(dataFolder))
+            dataFolder = ".";
 
-    Console.WriteLine(scriptData);
-    Console.WriteLine($"{'\n'}");
+        //  テストファイル選択
+        string scriptFile = ylib.consoleFileSelect(dataFolder, "*.sc");
+        string scriptData = ylib.loadTextFile(scriptFile);
+        if (scriptData == null || scriptData.Length <= 0) return;
 
-    KParse parse = new KParse(scriptData);
-    //parse.mTokenList.ForEach(p => Console.Write($"{p.mValue}[{p.mType}] "));
-    //Console.WriteLine($"{'\n'}");
-    parse.execute("main");
+        Console.WriteLine(scriptData);
+        Console.WriteLine($"{'\n'}");
+
+        KParse parse = new KParse(scriptData);
+        //parse.mTokenList.ForEach(p => Console.Write($"{p.mValue}[{p.mType}] "));
+        //Console.WriteLine($"{'\n'}");
+        parse.execute("main");
+
+        //  既定フォルダの保存
+        dataFolder = Path.GetDirectoryName(scriptFile);
+        KScript.Properties.Settings.Default.DataFolder = dataFolder;
+        KScript.Properties.Settings.Default.Save();
+    }
 }
